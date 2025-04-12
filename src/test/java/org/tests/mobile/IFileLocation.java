f7e955bdfa157224553c1faf0c0fe2253f3eddb9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-12 16:52:26 +0530

    damco assignment

--- src/test/java/org/tests/mobile/IFileLocation.java
+++ src/test/java/org/tests/mobile/IFileLocation.java
@@ -1,5 +1,5 @@
 package org.tests.mobile;
 
 public interface IFileLocation {
-    final String APK_FILE="src/test/resources/apk_file/Calculator.apk";
+    final String APK_FILE=System.getProperty("user.dir")+"\\src\\test\\resources\\apk_file\\Calculator.apk";
 }
