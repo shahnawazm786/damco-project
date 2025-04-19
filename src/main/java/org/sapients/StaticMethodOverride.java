44e16373e6ab86986e34185f1b649de61ee1df58
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 15:50:02 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/StaticMethodOverride.java
@@ -0,0 +1,15 @@
+package org.sapients;
+//Can we override static methods in java?
+//
+public class StaticMethodOverride {
+    static void show(){
+        System.out.println("show Method in parent class");
+    }
+
+}
+class parent extends  StaticMethodOverride{
+    static  void show(){
+        System.out.println("show method in child class");
+    }
+}
+
