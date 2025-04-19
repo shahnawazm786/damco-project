c15f66cbfd52dfcbc410a97ac2f80470483e4807
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 15:49:50 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/DemoOverride.java
@@ -0,0 +1,11 @@
+package org.sapients;
+// we can override the static method as well
+public class DemoOverride {
+    public static void main(String[] args) {
+        StaticMethodOverride st=new parent();
+        st.show();
+        parent.show();
+        StaticMethodOverride.show();
+
+    }
+}
