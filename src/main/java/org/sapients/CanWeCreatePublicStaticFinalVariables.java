4582c1367e3ba8497d1e5bada43f83afc8bd22ea
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 15:49:44 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/CanWeCreatePublicStaticFinalVariables.java
@@ -0,0 +1,12 @@
+package org.sapients;
+
+// Can we create public static final variables in java?
+public class CanWeCreatePublicStaticFinalVariables {
+    public static final String str="My name is java"; // we can create outside the class as well as
+    // in interface
+    public static void main(String[] args) {
+       // public static final String str="My name is java";
+        // public and static modifier not allowed here
+
+    }
+}
