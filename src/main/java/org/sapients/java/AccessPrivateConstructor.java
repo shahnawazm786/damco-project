7e20861a1653d04bea1b8d0d7ddd17aa9ecdb3af
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 16:39:19 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/java/AccessPrivateConstructor.java
@@ -0,0 +1,9 @@
+package org.sapients.java;
+
+public class AccessPrivateConstructor {
+    public static void main(String[] args) {
+        //CanConstructorBePrivate c=new CanConstructorBePrivate(10);
+        // outside the class can't be accessed but we can create the private constructor and can be
+        // use in same class
+    }
+}
