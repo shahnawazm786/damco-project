60248736203b0ea529944dc8924aca73821e8e8e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 16:04:53 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/CallConstructorInOtherClass.java
@@ -0,0 +1,8 @@
+package org.sapients;
+
+public class CallConstructorInOtherClass {
+    public static void main(String[] args) {
+        CallConstructorInOtherClassAndInSameClass c=new CallConstructorInOtherClassAndInSameClass();
+        c.show();
+    }
+}
