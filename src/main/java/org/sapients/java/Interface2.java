91a345acd119224dff445ffd1e6b0136f2eb044f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 16:54:10 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/java/Interface2.java
@@ -0,0 +1,13 @@
+package org.sapients.java;
+
+/*
+You have 2 interfaces and you declared 4 methods in 1st interface
+and 2 methods in 2nd interface
+and now you are implementing those 2 interfaces in your class,
+you would want to implement only few declared methods from those 2 interfaces
+so how you deal with this?
+ */
+public interface Interface2 {
+    void realme();
+    void oneplus();
+}
