5f87d0d0a1b7498ab9094eb91d3c51b44e5e3901
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 15:28:37 +0530

    damco assignment

---
+++ src/main/java/org/sapients/CheckNullKeyAllowInMap.java
@@ -0,0 +1,16 @@
+package org.sapients;
+
+import java.util.HashMap;
+import java.util.Map;
+// Question
+/*
+ Does Map supports null keys?
+ */
+public class CheckNullKeyAllowInMap {
+    public static void main(String[] args) {
+        Map<String,String> map=new HashMap<>();
+        map.put(null,"Arham");
+        map.put("Name","Hanan");
+        System.out.println(map);
+    }
+}
