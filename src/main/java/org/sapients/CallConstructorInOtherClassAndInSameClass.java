d95207832a00efe159e66a05f10d8b1743a53ff7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 16:09:47 +0530

    sapient question and answer

--- src/main/java/org/sapients/CallConstructorInOtherClassAndInSameClass.java
+++ src/main/java/org/sapients/CallConstructorInOtherClassAndInSameClass.java
@@ -2,14 +2,19 @@ package org.sapients;
 // How to call constructors of other classes and same class in this class
 // This is an example of constructor called in the same class
 public class CallConstructorInOtherClassAndInSameClass {
+    int value;
     void show(){
         System.out.println("Show method");
+        System.out.println("Value is"+this.value);
     }
     public CallConstructorInOtherClassAndInSameClass(){
-
+        this(0);
+    }
+    public CallConstructorInOtherClassAndInSameClass(int value){
+        this.value=value;
     }
     public static void main(String[] args) {
-        CallConstructorInOtherClassAndInSameClass c=new CallConstructorInOtherClassAndInSameClass();
+        CallConstructorInOtherClassAndInSameClass c=new CallConstructorInOtherClassAndInSameClass(10);
         c.show();
     }
 }
