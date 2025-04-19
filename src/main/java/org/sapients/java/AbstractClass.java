f0d99b475c35f6778664fb47e73e6dfb168dcf92
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 16:53:51 +0530

    sapient question and answer

---
+++ src/main/java/org/sapients/java/AbstractClass.java
@@ -0,0 +1,12 @@
+package org.sapients.java;
+
+public abstract class AbstractClass implements Interface1{
+    void htc(){
+        System.out.println("HTC");
+    }
+
+    @Override
+    public void realme(){
+        System.out.println("realme");
+    }
+}
