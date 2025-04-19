c9138813ae3457023162b23254691d74528cb0af
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-19 15:32:46 +0530

    damco assignment

--- src/main/java/org/sapients/CanWeUserConstructorStatic.java
+++ src/main/java/org/sapients/CanWeUserConstructorStatic.java
@@ -1,4 +1,10 @@
 package org.sapients;
-
+// Static constructor
+// Can we use declare constructor static in java
 public class CanWeUserConstructorStatic {
+    /*
+    public static CanWeUserConstructorStatic(){
+        we can't declare static constructor
+    }*/
+
 }
