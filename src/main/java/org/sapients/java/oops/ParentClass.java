20f12b6dbd1e3139c22f32bc52504aa6bafdd4f9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-29 06:30:42 +0530

    Roles and resposbility

---
+++ src/main/java/org/sapients/java/oops/ParentClass.java
@@ -0,0 +1,11 @@
+package org.sapients.java.oops;
+
+public class ParentClass {
+    int a=40;
+    public void display(int a){
+        System.out.println("value of a in parent class"+a);
+    }
+    public void display1(){
+        System.out.println("Value of a in parent class"+a);
+    }
+}
