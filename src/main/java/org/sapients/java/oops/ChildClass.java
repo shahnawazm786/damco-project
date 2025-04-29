7ca3c59daed1f2874f2186d5647784609d3429c9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-29 06:30:28 +0530

    Roles and resposbility

---
+++ src/main/java/org/sapients/java/oops/ChildClass.java
@@ -0,0 +1,9 @@
+package org.sapients.java.oops;
+
+public class ChildClass extends  ParentClass{
+    int a=60;
+    void display(){
+        System.out.println("value of a in child class"+a);
+    }
+
+}
