fb052c5ae6075259877efe76ac03e3c9b018e198
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-04-30 09:13:02 +0530

    Interview Preparation

--- src/main/java/org/sapients/java/strings/FilterSpecialCharacterFromString.java
+++ src/main/java/org/sapients/java/strings/FilterSpecialCharacterFromString.java
@@ -5,10 +5,25 @@ import java.util.List;
 
 public class FilterSpecialCharacterFromString {
     public static void main(String[] args) {
-
+        filter("m79&fg%3^");
+        removeSepcialCharacter("a##$$bcd%^789$$%%");
+        keepSpecialCharacter("a##$$bcd%^789$$%%  asd$$  ghj768");
     }
     static void filter(String sentence){
         List<Character> characters=new ArrayList<>();
-        for()
+        for(char c: sentence.toCharArray()){
+            if(!Character.isLetterOrDigit(c)){
+                characters.add(c);
+            }
+        }
+        System.out.println(characters);
+    }
+    static void removeSepcialCharacter(String sentence){
+        String cleaned=sentence.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(cleaned);
+    }
+    static void keepSpecialCharacter(String sentence){
+        String cleaned=sentence.replaceAll("[a-zA-Z0-9\\s]","");
+        System.out.println(cleaned);
     }
 }
