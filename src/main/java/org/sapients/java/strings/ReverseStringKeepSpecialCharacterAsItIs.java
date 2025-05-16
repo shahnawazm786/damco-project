b0149ed398ee7a7875918f6d15964c6da1404b2d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-05-16 08:06:32 +0530

    Interview Preparation

--- src/main/java/org/sapients/java/strings/ReverseStringKeepSpecialCharacterAsItIs.java
+++ src/main/java/org/sapients/java/strings/ReverseStringKeepSpecialCharacterAsItIs.java
@@ -2,7 +2,8 @@ package org.sapients.java.strings;
 
 public class ReverseStringKeepSpecialCharacterAsItIs {
     public static void main(String[] args) {
-        reverse("ab#de$klm");
+        //reverse("ab#de$klm");
+        reverse1("ab#de$klm");
     }
     static void reverse(String str){
         char[] chars=str.toCharArray();
@@ -24,5 +25,27 @@ public class ReverseStringKeepSpecialCharacterAsItIs {
         }
         System.out.println(chars);
     }
+
+    static void reverse1(String str){
+        char[] chars=str.toCharArray();
+        System.out.println(chars);
+        for(int i=0,j=chars.length-1;i<j;){
+            if(!(chars[i]>='A'| chars[i]<='Z' |chars[i]>='a'| chars[i]<='z' )){
+                i++;
+            }
+            else if(!(chars[j]>='A'| chars[j]<='Z' |chars[j]>='a'| chars[j]<='z' )){
+                j--;
+            }
+            else{
+                char c=chars[i];
+                chars[i]=chars[j];
+                chars[j]=c;
+                i++;
+                j--;
+            }
+        }
+        System.out.println(chars);
+    }
+
 
 }
