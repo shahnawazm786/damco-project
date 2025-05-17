dd1dacd887e8fbcc3c8c7b354f19880c986aac9a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-05-17 12:03:43 +0530

    codility

---
+++ src/main/java/codility/BinaryGap.java
@@ -0,0 +1,13 @@
+package codility;
+
+public class BinaryGap {
+    static int solution(int n){
+        String bString=Integer.toString(n,2);
+        System.out.println(bString);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        solution(9);
+    }
+}
