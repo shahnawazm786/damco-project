a9bc3043eb82f000448c75dd308ad6a47a5a2ff6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-05-17 20:40:25 +0530

    codility

--- src/main/java/codility/MissingNumber.java
+++ src/main/java/codility/MissingNumber.java
@@ -1,10 +1,12 @@
 package codility;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class MissingNumber {
     List<Integer> aList=new ArrayList<>();
+    HashSet<Integer> aHash=new HashSet<>();
     public  int Solution(int[] A){
         for(int i=1;i<=A.length+1;i++){
             aList.add(i);
@@ -14,8 +16,19 @@ public class MissingNumber {
         }
         return aList.get(0);
     }
+    public  int Solution1(int[] A){
+        for(int i=1;i<=A.length+1;i++){
+            aHash.add(i);
+        }
+        for(int i=0;i<A.length;i++){
+            aHash.remove(Integer.valueOf(A[i]));
+        }
+        return aHash.iterator().next();
+    }
     public static void main(String[] args) {
         MissingNumber mn=new MissingNumber();
         System.out.println(mn.Solution(new int[]{1,2,3,5}));
+        System.out.println(mn.Solution1(new int[]{1,2,3,5}));
+        System.out.println(mn.Solution1(new int[]{}));
     }
 }
