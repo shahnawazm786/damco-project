10e43dc36fcc8243e20f040e1d02b66a42a83627
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-05-17 19:44:43 +0530

    codility

--- src/main/java/codility/CyclicRotation.java
+++ src/main/java/codility/CyclicRotation.java
@@ -2,12 +2,14 @@ package codility;
 
 public class CyclicRotation {
     public int[] solution(int[] A,int K){
+        if(A.length>0){
         for(int i=0; i<K;i++){
             int lastValue=A[A.length -1];
             for(int j=A.length-2;j>=0;j--){
                 A[j+1]=A[j];
             }
             A[0]=lastValue;
+        }
         }
         return A;
     }
