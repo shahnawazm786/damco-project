5dbe2c7709bca2f0b8ad6917c7e4bc80e4d00e00
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-05-17 20:04:48 +0530

    codility

--- src/main/java/codility/OddOccuranceInArrays.java
+++ src/main/java/codility/OddOccuranceInArrays.java
@@ -1,16 +1,18 @@
 package codility;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class OddOccuranceInArrays {
     List<Integer> aList=new ArrayList<>();
+    HashSet<Integer> aHash=new HashSet<>();
     public int Solution(int[] A){
         //for(int i=0;i<A.length;i++){
         for(int a:A){
             //int a=A[i];
             if(aList.contains(a)){
-                aList.remove(Integer.valueOf(a));
+                aList.remove(a);
             }
             else {
                 aList.add(a);
@@ -19,8 +21,24 @@ public class OddOccuranceInArrays {
         }
         return aList.get(0);
     }
+    public int Solution1(int[] A){
+        //for(int i=0;i<A.length;i++){
+        for(int a:A){
+            //int a=A[i];
+            if(aHash.contains(a)){
+                aHash.remove(a);
+            }
+            else {
+                aHash.add(a);
+            }
+
+        }
+        return aHash.iterator().next();
+        //return aList.get(0);
+    }
     public static void main(String[] args) {
         OddOccuranceInArrays oi=new OddOccuranceInArrays();
         System.out.println(oi.Solution(new int[]{9,3,9,3,9,7,9}));
+        System.out.println(oi.Solution1(new int[]{9,3,9,3,9,7,9}));
     }
 }
